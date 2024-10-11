d6fe09599e7210ad5378d33b2b8d1e9aa7c81688
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-10-11 21:38:49 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'rt') as file:
     print(install_requires)
 
 setup(
-    name='airfoil',
+    name='foil',
     version=time.strftime('%Y.%m.%d.%H.%M.%S', time.localtime()),
     description='lib',
     long_description=long_description,
     long_description_content_type='text/markdown',  # если long_description = .md
     author='Daniil Andryushin',
     author_email='',
-    url='https://github.com/ParkhomenkoDV/airfoil.git',
+    url='https://github.com/ParkhomenkoDV/foil.git',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=install_requires,
